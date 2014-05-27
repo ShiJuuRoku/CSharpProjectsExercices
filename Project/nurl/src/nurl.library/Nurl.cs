263556bbf9cb3facb9a21ca0249ba7fee5bf95a0
ShiJuuRoku <truong_v13@egi.fr>
2014-05-27 14:25:07 +0200

    Nurl Get ok

---
+++ Project/nurl/src/nurl.library/Nurl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace nurl.library
+{
+    public class Nurl : INurl
+    {
+        private WebClient wClient = new WebClient();
+
+        public string Get(string url)
+        {
+            try
+            {
+                return this.wClient.DownloadString(url);
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.Error.WriteLine(ex.StackTrace);
+                throw;
+            }
+            catch(WebException ex)
+            {
+                Console.Error.WriteLine(ex.StackTrace);
+                throw;
+            }
+            catch(NotSupportedException ex)
+            {
+                Console.Error.WriteLine(ex.StackTrace);
+                throw;
+            }
+        }
+
+        public void GetAndSave(string url, string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<decimal> Test(string url, int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TestAvg(string url, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project/nurl/test/nurl.test/NurlTest/NurlTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nurl.library;
+
+namespace nurl.test.NurlTest
+{
+    [TestClass]
+    public class NurlGetTest
+    {
+        INurl nurl = new Nurl();
+
+        [TestMethod]
+        public void GetSuccessful()
+        {
+            var url = "B:/Etudes/ESGI/4AL2/Langage C#/CSharpProjectsExercices/Project/nurl/test/nurl.test/Pages/page1.html";
+            Assert.AreEqual("<h1>Hello World</h1>", nurl.Get((url)));
+        }
+
+        [TestMethod]
+        public void GetFailed()
+        {
+            var url = "B:/Etudes/ESGI/4AL2/Langage C#/CSharpProjectsExercices/Project/nurl/test/nurl.test/Pages/page1.html";
+            Assert.AreNotEqual("Hello World</h1>", nurl.Get((url)));
+        }
+    }
+}
